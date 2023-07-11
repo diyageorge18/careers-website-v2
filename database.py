d2708d2a743ebfb9958750e7f207a91a239386aa
diyageorge18 <133945601+diyageorge18@users.noreply.github.com>
2023-07-11 18:19:07 +0000

    DB connection

--- database.py
+++ database.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine, text
 
-db_connection_string = "mysql+pymysql://dzz718f3s2105wfmun4h:pscale_pw_niVSw4KIq0klhFFZ1EDRm4ALznTep8uWCI8IhFhS6rL@aws.connect.psdb.cloud/joanncarrers?charset=utf8mb4"
-engine = create_engine(db_connection_string,
-                       connect_args={"ssl": {
-                         "ca": "/etc/ssl/cert.pem"
-                       }})
+db_connection_string = "mysql+pymysql://94rv29ffg1ld77x6gg8t:pscale_pw_JdrumEXSL5HOy2amDkk2dZqH1bMfBVOSG9hZ4hsIb5k@aws.connect.psdb.cloud/joanncarrers?charset=utf8mb4"
 
-with engine.connect() as conn:
-  result = conn.execute(text("select *from jobs"))
-  print(result.all())
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+     "ssl": {
+       "ssl ca": "/etc/ssl/cert.pem"
+     }
+  })
+
+with engine.connect() as connection:
+  result = connection.execute(text("select * from jobs"))
+  for row in result:
+        print("jobs:", row.title)
+ 
